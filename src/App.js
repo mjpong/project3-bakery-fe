ec187f90a741ce5772e13a04b951309690e70b08
mjpong <jolenepong@gmail.com>
2021-07-15 02:51:57 +0000

    initial commit after create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Testing Bakery Frontend</h1>
+
+    </React.Fragment>
+
+
+  );
+}
+
+export default App;
